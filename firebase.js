6158b1c26962533f5dfcc58b003d3c67508fadc3
mumernisar <umernisar3854@gmail.com>
2024-11-30 11:27:59 +0500

    update secrets

--- firebase.js
+++ firebase.js
@@ -1,29 +1,10 @@
-require("dotenv").config();
 const admin = require("firebase-admin");
-
-// const serviceAccount = {
-//   type: process.env.FIREBASE_TYPE,
-//   project_id: process.env.FIREBASE_PROJECT_ID,
-//   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-//   private_key: process.env.FIREBASE_PRIVATE_KEY,
-//   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-//   client_id: process.env.FIREBASE_CLIENT_ID,
-//   auth_uri: process.env.FIREBASE_AUTH_URI,
-//   token_uri: process.env.FIREBASE_TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-//   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-//   universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-// };
-
+const PATH = process.env.FIREBASE_SERVICE_ACCOUNT || "./firebase-service-account.json";
+const serviceAccount = require(PATH);
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
-
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ikms-d78d2.firebaseio.com",
 });
 
 const db = admin.firestore();
